59f80336987789333d032e3ec60ace82291a3f5d
Alireza Zahiri <itsalireza.zr@gmail.com>
2024-09-14 23:21:57 +0330

    ✅ solved Leetcode 108th  problem

---
+++ Daily Challenges/Convert Sorted Array to Binary Search Tree/main.go
@@ -0,0 +1,22 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	mid := len(nums) / 2
+	root := &TreeNode{Val: nums[mid]}
+	root.Left = sortedArrayToBST(nums[:mid])
+	root.Right = sortedArrayToBST(nums[mid+1:])
+	return root
+}
+
+func main() {
+
+}
